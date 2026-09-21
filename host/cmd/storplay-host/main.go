4468be0db2fcd4629771b248f572471d9e9aa35d
DaNiLoStOrM <storm.danilo@gmail.com>
2026-09-21 11:14:41 -0300

    feat(host): configure local Sunshine adapter

--- host/cmd/storplay-host/main.go
+++ host/cmd/storplay-host/main.go
@@ -5,18 +5,27 @@ import (
 	"log"
 
 	"github.com/danilostorm/storplay/host/internal/signaling"
+	"github.com/danilostorm/storplay/host/internal/sunshine"
 )
 
 func main() {
 	addr := flag.String("listen", "0.0.0.0:47990", "HTTP/WebSocket listen address")
 	stun := flag.String("stun", "", "optional STUN URL, for example stun:stun.example.com:3478")
 	webDir := flag.String("web-dir", "", "optional directory containing the built StorPlay web client")
+	sunshineURL := flag.String("sunshine", "http://127.0.0.1:47989", "Sunshine/GameStream HTTP base URL; empty disables probing")
 	flag.Parse()
 
+	var sunshineClient *sunshine.Client
+	if *sunshineURL != "" {
+		sunshineClient = sunshine.New(*sunshineURL, sunshine.NewUniqueID())
+		log.Printf("Sunshine adapter: probing %s on demand", *sunshineURL)
+	}
+
 	server := signaling.Server{
-		Addr:    *addr,
-		StunURL: *stun,
-		WebDir:  *webDir,
+		Addr:     *addr,
+		StunURL:  *stun,
+		WebDir:   *webDir,
+		Sunshine: sunshineClient,
 	}
 
 	log.Printf("open %s/healthz to verify the host", signaling.DescribeURL(*addr))
